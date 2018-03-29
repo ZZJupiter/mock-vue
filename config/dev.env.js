2b51d8a6f9f7b50ea99bd571bcd0f30a74e00a79
Zz <zlyxzq@163.com>
2018-03-29 18:16:57 +0800

    1.修改打包的环境地质

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,6 @@
 module.exports = {
     NODE_ENV: '"development"',
-    BASE_API: '"http://localhost:7001"',
+    // BASE_API: '"http://localhost:7001"',
+    BASE_API: "\"http://115.159.159.126:7001\"",
     APP_ORIGIN: '"https://wz.com"'
 }
--- config/index.js
+++ config/index.js
@@ -37,5 +37,20 @@ module.exports = {
         // In our experience, they generally work as expected,
         // just be aware of this issue when enabling this option.
         cssSourceMap: false
+    },
+    prod: {
+        env: require("./prod.env"),
+        port: 9001,
+        autoOpenBrowser: true,
+        assetsSubDirectory: "static",
+        assetsPublicPath: "/",
+        staticPath: "/static/",
+        proxyTable: {},
+        // CSS Sourcemaps off by default because relative paths are "buggy"
+        // with this option, according to the CSS-Loader README
+        // (https://github.com/webpack/css-loader#sourcemaps)
+        // In our experience, they generally work as expected,
+        // just be aware of this issue when enabling this option.
+        cssSourceMap: false
     }
 }
